6c1550baa37e8b5e86713b05e6e1ea22dad91ff5
itsFiz <hapizzz343@gmail.com>
2023-10-31 13:55:33 +0800

    latest

--- src/module8/ReDemo.java
+++ src/module8/ReDemo.java
@@ -12,5 +12,36 @@ public class ReDemo {
             System.out.println("Match found");
         else
             System.out.println("Not match");
+
+        ReDemo.icValidator();
+        ReDemo.emailValidator();
+        ReDemo.stringSplitter();
+    }
+
+    public static void icValidator(){
+        Pattern pattern = Pattern.compile("[0-9]{6}-10-[0-9]{4}");
+        Matcher matcher = pattern.matcher("009876-10-2040");
+        boolean matchFound = matcher.find();
+        if (matchFound)
+            System.out.println("Match found");
+        else
+            System.out.println("Not match");
+
+    }
+
+    public static void emailValidator(){
+        Pattern pattern = Pattern.compile("[A-Za-z0-9]{1,}@[A-Za-z0-9]{1,}(.[A-Za-z)]{2,3})+");
+        Matcher matcher = pattern.matcher("fiz@gg.com");
+        boolean matchFound = matcher.find();
+        if (matchFound)
+            System.out.println("Match found");
+        else
+            System.out.println("Not match");
+
+    }
+
+    public static void stringSplitter(){
+        Pattern pattern = Pattern.compile("");
+        String str = "";
     }
 }
